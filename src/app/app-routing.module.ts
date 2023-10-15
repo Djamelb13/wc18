1f2e76d7fb77b9e383a4b45ce2f4a68227b4264b
Djaml <dwwm-djamel-bennabou@mode83.onmicrosoft.com>
2023-10-15 22:54:32 +0200

    ajout test liste option

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { TeamComponent } from './team/team.component';
+import { ProfileComponent } from './profile/profile.component';
+import { GroupComponent } from './group/group.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:LoginComponent},
+  {path: 'home', component:HomeComponent},
+  {path: 'team', component:TeamComponent},
+  {path: 'profile', component:ProfileComponent},
+  {path: 'group', component:GroupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/home/article/article.component.ts
+++ src/app/home/article/article.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./article.component.scss']
 })
 export class ArticleComponent {
+  articles: any[] = [
+    {
+      title: "Article 1",
+      author: "Auteur 1",
+      content: "Ceci est le contenu de l'article 1. Il peut contenir des informations intéressantes sur divers sujets."
+    },
+    {
+      title: "Article 2",
+      author: "Auteur 2",
+      content: "Ceci est le contenu de l'article 2. Il peut contenir des informations intéressantes sur divers sujets."
+    },
+    {
+      title: "Article 3",
+      author: "Auteur 3",
+      content: "Ceci est le contenu de l'article 3. Il peut contenir des informations intéressantes sur divers sujets."
+    }
+  ];
 
+  constructor() { }
 }
