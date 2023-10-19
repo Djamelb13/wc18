df91a985413d76823bb152adb043911b71bec655
Academy <academy@iMac-de-Academy.local>
2023-10-19 16:52:18 +0200

    branch a jour

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,16 @@
 // header.component.ts
-import { Component, Input, ViewChild, OnInit } from '@angular/core';
+import { Component, Input, ViewChild, OnInit, OnDestroy } from '@angular/core';
 import { MatMenuTrigger } from '@angular/material/menu';
 import { Router } from '@angular/router';
 import { DataService } from '../data.service';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   title!: string;
   titreDuGroupeOuPays!: string;
   titreDuGroupeOuPays$!: Observable<string>;
@@ -18,15 +18,24 @@ export class HeaderComponent implements OnInit {
   selectedGroupe: any;
   @ViewChild(MatMenuTrigger) trigger?: MatMenuTrigger;
   previousSelectedGroup!: string;
+  private titreSubscription: Subscription = new Subscription;
 
-  constructor(private router: Router, private dataService: DataService) {
+  constructor(
+    private router: Router,
+    private dataService: DataService
+  ) {
     this.isHome = false;
   }
 
   ngOnInit() {
-    this.dataService.titreDuGroupeOuPays$.subscribe((titre: string) => {
+    this.titreSubscription = this.dataService.titreDuGroupeOuPays$.subscribe((titre: string) => {
       this.titreDuGroupeOuPays = titre;
+      this.previousSelectedGroup = titre;
     });
+  }
+
+  ngOnDestroy() {
+    this.titreSubscription.unsubscribe();
   }
 
   groupesDePays = this.dataService.getGroupesDePays();
@@ -34,14 +43,13 @@ export class HeaderComponent implements OnInit {
   onGroupSelected(selectedValue: string) {
     const groupeSelectionne = selectedValue;
     this.dataService.updateTitreDuGroupeOuPays(groupeSelectionne);
-    this.previousSelectedGroup = groupeSelectionne; // Stockez le groupe précédemment sélectionné
     this.router.navigate(['/group', groupeSelectionne]);
   }
 
   goBack() {
     if (this.previousSelectedGroup) {
       this.dataService.updateTitreDuGroupeOuPays(this.previousSelectedGroup);
-      this.router.navigate(['/group', this.previousSelectedGroup]); // Naviguez vers la page précédemment sélectionnée
     }
+    window.history.back();
   }
 }
